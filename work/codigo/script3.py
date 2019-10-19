52e3420a6754a6c52e01291f507cd163c8151ed5
Joseph <josephccaceres@gmail.com>
2019-10-19 04:08:07 -0600

    cambios

--- work/codigo/script3.py
+++ work/codigo/script3.py
@@ -37,9 +37,9 @@ df.createOrReplaceTempView("table1")
 
 #REPORTE 3------------------------------------------------------------------------------------
 df4 = spark.sql("SELECT Pais, ROUND(sum(`Ganancia Total`),3) as Ganancia FROM table1 "
-                "WHERE (Country == 'Guatemala' OR Country == 'Costa Rica' OR Country == 'El Salvador' OR Country == 'Honduras' OR Country == 'Nicaragua' OR Country == 'Panama') AND Departamento == 'Clothes'"
+                "WHERE (Country == 'Guatemala' ) AND Departamento == 'Clothes'"
           "Group By Pais")
 df4.show()
-
+#OR Country == 'Costa Rica' OR Country == 'El Salvador' OR Country == 'Honduras' OR Country == 'Nicaragua' OR Country == 'Panama'
 data = [go.Pie(labels=df4.toPandas()['Pais'],values=df4.toPandas()['Ganancia'])]
 plot(data, filename="/home/jovyan/work/graficas/Reporte3.html")
